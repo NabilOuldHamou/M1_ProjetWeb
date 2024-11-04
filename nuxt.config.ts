a57783dd7f480013634d7c82290a7c3ae68ca001
Nabil Ould Hamou <ouldhamounabil@gmail.com>
2024-11-04 21:14:23 +0100

    Hero page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,15 @@
 import Aura from '@primevue/themes/aura'
 
 export default defineNuxtConfig({
+  tailwindcss: {
+    cssPath: '~/assets/tailwind.css',
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    '@primevue/nuxt-module'
+    '@primevue/nuxt-module',
+    '@nuxt/image'
   ],
   primevue: {
     options: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-    <section>
-        <h1 class="text-6xl font-bold">Transformez vos entraînements</h1>
-        <h1 class="text-6xl font-bold">grâce à l'IA</h1>
+    <section class="container mx-auto hero-size">
+        <div class="flex items-center w-full h-full justify-center gap-10">
+            <div class="h-1/3">
+                <h1 class="text-5xl font-bold">Transformez vos entraînements</h1>
+                <h1 class="text-5xl font-bold">grâce à l'IA</h1>
+            </div>
+            <NuxtImg sizes="25vw" src="/hero-image.svg" />
+        </div>
     </section>
 </template>
   
